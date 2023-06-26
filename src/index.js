cf38a2f64e21740f23f43044f7a3f898e129f145
AkiraQZ <vboyrkin@gmail.com>
2023-06-26 23:01:13 +0800

    init:add index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//import our custom modules and node modules
+import * as os from "node:os";
+import * as process from 'node:process';
+import { homedir } from "os";
+
+
+//Enter to home directory
+process.chdir(homedir());
+
+//Get name from username=name
+const nameParse = Object.fromEntries(
+    process.argv.slice(2).map((argument)=> {
+        const [key, value] = argument.split('=');
+        return [key, value];
+    })
+)
+//Show welcome massage (if username=Null, show noName)
+const name = nameParse['--username'] ? nameParse['--username'] : 'noName';
+console.log(`Welcome to the File Manager, ${name}!`);

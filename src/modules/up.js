e66b6de8ab7d1fc5a5e79381dc72eaa240e78e79
AkiraQZ <vboyrkin@gmail.com>
2023-06-27 01:03:10 +0800

    feat: add up.js

--- src/modules/up.js
+++ src/modules/up.js
@@ -1,1 +1,15 @@
-//for push!!!
+import * as process from 'node:process';
+import currDirect from '../helper/currDirect';
+
+export default async function up() {
+    try {
+        //change directory to upper
+        process.chdir('..');
+        //display current directory
+        currDirect();
+    } catch (error) {
+        //throw error
+        err();
+    }
+    
+}

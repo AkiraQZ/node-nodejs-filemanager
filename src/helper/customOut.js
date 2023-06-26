8f54f8258a6b09ade2e3cd7888908ff0d673f7df
AkiraQZ <vboyrkin@gmail.com>
2023-06-26 23:17:51 +0800

    init: add customOut

---
+++ src/helper/customOut.js
@@ -0,0 +1,12 @@
+import {Writable} from 'node:stream';
+
+//function for output by stream
+export function customOut() {
+    return new Writable({
+        decodeStrings: false, 
+        write(chunk, _, callback){
+            console.log(chunk);
+            callback()
+        },
+    })
+}
